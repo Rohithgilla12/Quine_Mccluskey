3d9604a0e07ec1ca1e2f3db3776d5524ffa3c7a4
Abhinav Reddy Nimma <abhinavreddynimma@gmail.com>
2017-08-26 15:05:24 +0530

    small changes , removing runtime errors etc

--- main.py
+++ main.py
@@ -70,15 +70,17 @@ def combine_s2(x1,x2):  #x1,x2 are binary terms from lists n_0 etc
         else :
 
             return [False,None]
-implicants=[]
+primt_implicants=[]
 #Add a try block if n_1 has no elements it gives out a error
 for i1 in n_1: #For each element in n_1
     for i2 in n_2: #Comparing with each element in n_2
         temp=combine_s2(i1,i2)
         if temp[0] is True:
-            i=list(i)
-            i[temp[1]]='_' #Postion to be daashed
-            implicants.append(''.join(i))
+            i1=list(i1)
+            i1[temp[1]]='_' #Postion to be daashed
+            prime_implicants.append(''.join(i1))
+
+print prime_implicants
 
 # ''' rohith plz add code here such that x1 is selected from n_x and x2 is selected from n_x+1,and should cover all sets'''
 #
