811b67437265df27a850a368554706180e3b102d
Abhinav Reddy Nimma <abhinavreddynimma@gmail.com>
2017-08-28 18:16:53 +0530

    time pass

--- main.py
+++ main.py
@@ -273,10 +273,18 @@ for i in implicants :
                 #print j
 #print pk
 e_prime_implicantsa.extend(pk)
-print e_prime_implicantsa
+#print e_prime_implicantsa
 ''' for removing common e_prime_implicantsa '''  #this works fine , donot make changes
-import collections
-compare = lambda x, y: collections.Counter(x) == collections.Counter(y)  #dont know what it does , just took it from stackoverflow
+# import collections
+# compare = lambda x, y: collections.Counter(x) == collections.Counter(y)  #dont know what it does , just took it from stackoverflow
+# new_epi=[]
+# i1=0
+# for i in e_prime_implicantsa:
+#     for i1 in range(len(e_prime_implicantsa)):
+#         if compare(i,e_prime_implicantsa[i]) is False:
+#             new_epi.append(i)
+
+'''
 i=0
 j=0
 for i in e_prime_implicantsa:
@@ -286,5 +294,5 @@ for i in e_prime_implicantsa:
             if comp==True:
                  del e_prime_implicantsa[e_prime_implicantsa.index(j)]
 #print e_prime_implicantsa
-
+'''
 print e_prime_implicantsa
